f48f5cd7180b201fc5c8907e69ea9e05dfeb977a
Preethi S <84867260+86103Preethi@users.noreply.github.com>
2025-07-16 08:57:28 +0530

    video added

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,6 +6,11 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+
 
 import food from '../assets/food-thumbnail.jpg';
 import portfolio from '../assets/portfolio-thumbnail.png';
@@ -13,41 +18,65 @@ import data from '../assets/data visualization - thumbnail.jpg';
 import church from '../assets/church - thumbnail.jpeg';
 import todolist from '../assets/to do list - thumbnail.jpg';
 
+import dashVideo from '../assets/videos/dashboard video.mp4';
+import foodVideo from '../assets/videos/foodcart video.mp4';
+
+
 const workData = [
    {
     title: 'Church Website',
     image: church,
     description: 'Developed a clean, responsive website for a local church with pages for sermons, events, and contact, using HTML, CSS, Tailwind CSS and React.',
-    certificate: 'https://www.rakshaprayerhouse.org'
+    certificate: 'https://www.rakshaprayerhouse.org',
+    video:dashVideo
   },
   {
     title: 'Food Website',
     image: food,
     description: 'Created an interactive food ordering website with dynamic menus and cart features using React, optimized for both desktop and mobile devices.',
     certificate: 'https://github.com/86103Preethi/food-cart',
+    video:foodVideo
   },
   {
     title: 'Data Visualization Dashboard',
     image: data,
-    description: 'Built a dynamic dashboard to visualize complex datasets using React and Chart.js, including filters, charts, and analytics widgets.',
+    description: 'Built a dynamic dashboard to visualize complex datasets using React and rechart libraries, including filters, charts, and analytics widgets.',
     certificate: '#',
+    video:dashVideo
   },
   {
     title: 'Portfolio Website',
     image: portfolio,
     description: 'Designed and developed a fully responsive personal portfolio using React and Tailwind CSS to showcase skills, projects, and contact information.',
     certificate: 'https://github.com/86103Preethi/port-folio',
+    video:dashVideo
   },
     {
     title: 'To do list',
     image: todolist,
     description: 'A basic to do list for your daily routine.',
     certificate: 'https://github.com/86103Preethi/To-do-list---react',
+    video:dashVideo
   },
 ];
 
 
 const Work = () => {
+
+  const [open, setOpen] = React.useState(false);
+  const [selectedVideo, setSelectedVideo] = React.useState(null);
+
+  const handleOpen = (video) => {
+    setSelectedVideo(video);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setSelectedVideo(null);
+  };
+
+
   return (
     <div name="project" className="w-full bg-black text-[#ccd6f6] py-16 px-5">
       <div className="max-w-[1000px] mx-auto text-center">
@@ -61,7 +90,7 @@ const Work = () => {
         <Grid container spacing={4} justifyContent="center">
           {workData.map((item, index) => (
             <Grid item xs={12} sm={6} md={4} key={index}>
-              <Card sx={{ maxWidth: 345, mx: 'auto', backgroundColor: '#1f1f1f', color: 'white' }}   className='hover:opacity-70 hover:scale-110 transition-all duration-500'>
+              <Card sx={{ maxWidth: 345, mx: 'auto', backgroundColor: '#1f1f1f', color: 'white' }}   className='hover:opacity-70'>
                 <CardMedia
                   component="img"
                   height="160"
@@ -76,6 +105,8 @@ const Work = () => {
                     {item.description}
                   </Typography>
                 </CardContent>
+
+                <div className='flex justify-between'>
                 <CardActions>
                   <Button
                     size="small"
@@ -87,12 +118,47 @@ const Work = () => {
                     Source Code
                   </Button>
                 </CardActions>
+
+                <CardActions>
+                  <Button
+                    size="small"
+                    variant="outlined"
+                    onClick = {() => handleOpen(item.video)}
+                    sx={{ color: '#c76acb', borderColor: '#c76acb' }}
+                  >
+                    Sample Video
+                  </Button>
+                </CardActions>
+                </div>
+
+
+
               </Card>
             </Grid>
           ))}
         </Grid>
       </div>
+    
+
+    <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
+      <div className="flex justify-end p-2">
+        <IconButton onClick={handleClose}>
+          <CloseIcon />
+        </IconButton>
+      </div>
+      <DialogContent>
+        {selectedVideo && (
+          <video controls style={{ width: '100%' }}>
+            <source src={selectedVideo} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        )}
+      </DialogContent>
+    </Dialog>
+
     </div>
+
+    
   );
 };
 
